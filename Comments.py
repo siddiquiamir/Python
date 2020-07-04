460151ea61f9b83ac91983c1b0d8a76c95ca868e
Aamir Siddiqui <aamirsiddiqui196@gmail.com>
2020-07-04 17:50:35 +0530

    First Version

---
+++ Comments.py
@@ -0,0 +1,53 @@
+# Implicit Line condition
+a= [1,2,3]
+
+a= [1,
+    2,
+    3]
+
+print(a)
+
+a= [1, # item 1
+    2, # item 2
+]
+print(a)
+
+a= (1  #comment
+    ,2 # comment
+    ,3 # comment
+    )
+print(a)
+
+a= {"key1": 1 # value for key 1
+    , "key2": 2 # value for key 2
+    }
+print(a)
+
+def my_func(a, # this is a
+            b, # this is b
+            c # this is c
+ ):
+    print(a,b,c)
+
+my_func(10,20,30)
+
+# Explicit Line Condition
+a= 10
+b= 20
+c= 30
+
+if a > b \
+    and b > c \
+        and c > a :
+    print("This is True")
+else:
+    print("This is false")
+
+
+# Print multi line string
+def my_func():
+    a= '''This is a multi line string
+Keep writing'''
+    return a
+
+print(my_func())
