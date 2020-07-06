6352008feef6b6325d32a43bc5a2dac42d556b31
Aamir Siddiqui <aamirsiddiqui196@gmail.com>
2020-07-06 22:39:13 +0530

    first version

--- Comments.py
+++ Comments.py
@@ -54,4 +54,49 @@ print(my_func())
 
 account_id = 1001
 print(account_id)
-print(account_id)
+
+# If condition
+a= 6
+if a > 10:
+    print(True)
+else:
+    print(False)
+
+# Nested if statement
+a= 10
+
+if a < 5:
+    print("a < 5")
+else:
+    if a < 10:
+        print("5 >= a < 10")
+    else:
+        print(" a >= 10")
+
+# Elif condition
+a= 10
+if a < 5:
+    print("Less than 5")
+elif a < 6:
+    print("Less than 6")
+elif a < 7:
+    print("Less than 7")
+else:
+    print("Greater than 10")
+
+a=100
+b= "a < 5" if a < 5 else "a > 5"
+print(b)
+
+# Directly print the condition
+print("a < 5" if a < 5 else "a > 5")
+
+# Function
+def my_func(a,b):
+    return a*b
+
+print(my_func(2,3))
+
+print(my_func("you",3))
+
+print(my_func([1,2,3],3))
